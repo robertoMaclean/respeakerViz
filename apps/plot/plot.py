7cae31c0a38da3e7e6a8238f203412e86493201c
robreznor <nemesis_nin@hotmail.com>
2018-04-13 21:22:29 -0300

    fix plots

--- apps/plot/plot.py
+++ apps/plot/plot.py
@@ -1,6 +1,8 @@
 import csv
 import wave
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import networkx as nx
 from io import StringIO
@@ -26,51 +28,58 @@ class Plot(object):
 		interTimes = [[],[],[],[]]
 		timeActivity = []
 		lastPosition = -1
+		silence = 0
 		print(self.__outputPath)
 		reader = csv.DictReader(self.__file, delimiter=";")
 		for row in reader:
 			if int(row['speak']):
 				if int(row['direction']) != lastPosition:
 					if lastPosition != -1:
+						for i in range(silence):
+							self.__activityContinuos[lastPosition].pop()
 						interTimes[lastPosition].append(timeActivity)
 						timeActivity = []
 						self.FindUsersInteraction(lastPosition+1, int(row['direction'])+1)
 				timeActivity.append(float(row['seconds']))
 				lastPosition = int(row['direction'])
 				self.__activity[lastPosition].append(float(row['seconds']))
-			if lastPosition != -1:
-				self.__activityContinuos[lastPosition].append(float(row['seconds']))
+				silence = 0
+			else:
+				silence += 1
+			
+			self.__activityContinuos[lastPosition].append(float(row['seconds']))
 			self.__time = row['seconds']
 
 	def UsersSpeak(self):
 		#figure = plt.gcf() # get current figure
 		#figure.set_size_inches(12, 5, forward=True)
-		#plt.figure(figsize=(12, 4))
+		plt.figure(figsize=(12, 4))
 		plt.subplot(2,1,1)
-		plt.xticks(np.arange(0, float(self.__time)+1, 10))
+		div = float(self.__time)*0.12
+		plt.xticks(np.arange(0, float(self.__time)+1, int(float(self.__time)/div)))
 		plt.yticks([1],["Voz activa"])
-		plt.vlines(self.__activity[0], 0, 1, linewidth=0.05, label='Usuario 1', color='red')
-		plt.vlines(self.__activity[1], 0, 1, linewidth=0.05, label='Usuario 2', color='blue')
-		plt.vlines(self.__activity[2], 0, 1, linewidth=0.05, label='Usuario 3', color='green')
-		plt.vlines(self.__activity[3], 0, 1, linewidth=0.05, label='Usuario 4', color='orange')
+		linewidth = float(self.__time)/300
+		plt.vlines(self.__activity[0], 0, 1, linewidth=linewidth, label='Usuario 1', color='red')
+		plt.vlines(self.__activity[1], 0, 1, linewidth=linewidth, label='Usuario 2', color='blue')
+		plt.vlines(self.__activity[2], 0, 1, linewidth=linewidth, label='Usuario 3', color='green')
+		plt.vlines(self.__activity[3], 0, 1, linewidth=linewidth, label='Usuario 4', color='orange')
 		plt.xlabel("tiempo (segundos)") 
 		plt.title("Activación de voz con silencios")
 		plt.subplot(2,1,2)
 		plt.xticks(np.arange(0, float(self.__time)+1, 10))
 		plt.yticks([1],["Voz activa"])
-		plt.vlines(self.__activityContinuos[0], 0, 1, linewidth=0.05, label='Usuario 1', color='red')
-		plt.vlines(self.__activityContinuos[1], 0, 1, linewidth=0.05, label='Usuario 2', color='blue')
-		plt.vlines(self.__activityContinuos[2], 0, 1, linewidth=0.05, label='Usuario 3', color='green')
-		plt.vlines(self.__activityContinuos[3], 0, 1, linewidth=0.05, label='Usuario 4', color='orange')
+		plt.vlines(self.__activityContinuos[0], 0, 1, linewidth=linewidth, label='Usuario 1', color='red')
+		plt.vlines(self.__activityContinuos[1], 0, 1, linewidth=linewidth, label='Usuario 2', color='blue')
+		plt.vlines(self.__activityContinuos[2], 0, 1, linewidth=linewidth, label='Usuario 3', color='green')
+		plt.vlines(self.__activityContinuos[3], 0, 1, linewidth=linewidth, label='Usuario 4', color='orange')
 		plt.xlabel("tiempo (segundos)") 
-		plt.title("Activación de voz sin silencios")
-		#plt.tight_layout()
-		
+		plt.title("Activación de voz sin silencios")		
 		#plt.legend(frameon=True, framealpha=0.6)
 		leg = plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.5), fancybox=False, shadow=False, 
 				ncol=5)
 		for line in leg.get_lines():
 			line.set_linewidth(4.0)
+		plt.tight_layout()
 		plt.savefig(self.__outputPath+'/users_speak.png')
 		#plt.show()
 		plt.close()
@@ -90,10 +99,22 @@ class Plot(object):
 			self.__relations[5] += 1
 
 	def UsersInteraction(self):
+		node_speak = []
+		pix_max = 2000
+		pix_min = 500
+		func_max = []
+		func_min = []
+		maximo = max([len(self.__activity[0]),len(self.__activity[1]),len(self.__activity[2]),len(self.__activity[3])])
+		print(maximo)
+		for i in range(0, len(self.__activity)):
+			print(len(self.__activity[i]))
+			node_speak.append(len(self.__activity[i]))
+			func_max.append(int((float(len(self.__activity[i]))/maximo)*(pix_max-pix_min)+10))
+		print(func_max)
 		edgelist = [(1,2),(1,3),(1,4),(2,3),(2,4),(3,4)]
 		G=nx.Graph(edgelist)
 		pos=nx.circular_layout(G)
-		nx.draw(G,pos,node_color='#A0CBE2',edge_color=self.__relations,width=4,edge_cmap=plt.cm.Blues,with_labels=True)
+		nx.draw(G,pos,node_color='#A0CBE2',node_size=func_max, edge_color=self.__relations,width=4,edge_cmap=plt.cm.Blues,with_labels=True)
 		#plt.show()
 		plt.savefig(self.__outputPath+'/users_interaction.png')
 		plt.close()
