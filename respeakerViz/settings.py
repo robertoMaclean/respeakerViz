11c314f9a971e80ac938c4c3580fa33d184f02d1
robreznor <r.maclean.m@gmail.com>
2018-05-15 20:45:17 -0400

    change database

--- respeakerViz/settings.py
+++ respeakerViz/settings.py
@@ -81,13 +81,24 @@ WSGI_APPLICATION = 'respeakerViz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'Respeaker',
+#         'USER': 'postgres',
+#         'PASSWORD': 'teenage',
+#         'HOST':'localhost',
+#         'PORT': 5432,
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Respeaker',
-        'USER': 'postgres',
-        'PASSWORD': 'teenage',
-        'HOST':'localhost',
+        'NAME': 'ccvnpffr',
+        'USER': 'ccvnpffr',
+        'PASSWORD': 'g_ISkP4VVfO3fS3iTE-mDPGozcMReeOb',
+        'HOST':'tantor.db.elephantsql.com',
         'PORT': 5432,
     }
 }
